435217f00547ba72b449ef5e2566d829ebaa0b02
TheBigTing <mvobigting@gmail.com>
2025-11-04 12:50:31 -0800

    updated

--- polyVHDL/rej.vhd
+++ polyVHDL/rej.vhd
@@ -10,24 +10,26 @@ entity rej is
         enable : in std_logic;
         len : in unsigned (31 downto 0);
         buflen : in unsigned (31 downto 0);
+        ctr : out unsigned (7 downto 0);
 
-        ready : out std_logic := '0'
+        ready : out std_logic := '0';
 
-    -- --RAM I/O
-    --     --Inputs
-    --     ram_out_a : in RAM_OUT;
-    --     ram_out_buf : in RAM_OUT;
-    --     --Outputs
-    --     ram_in_a  : out RAM_IN; 
-    --     ram_in_buf  : out RAM_IN
+    --RAM I/O
+        --Inputs
+        ram_out_buf : in RAM_OUT;
+        --Outputs
+        ram_in_a  : out RAM_IN; 
+        ram_in_buf  : out RAM_IN
     );
 
 end rej;
 
 architecture RTL of rej is
 
-    signal ctr, pos : std_logic_vector(31 downto 0) := (others => '0');
-    signal t0, t1 : std_logic_vector(7 downto 0) := (others => '0');
+    signal ctr_s : std_logic_vector(7 downto 0) := (others => '0');
+    signal pos_s : std_logic_vector(8 downto 0) := (others => '0');
+    signal t0_s, t1_s : std_logic_vector(7 downto 0) := (others => '0');
+    constant ETA : unsigned(2 downto 0) := "111";
 
     type state is (processing, done);
     signal state_s : state := done;
@@ -35,19 +37,36 @@ architecture RTL of rej is
 begin
 
     process (clk)
+    
+    variable ctr_v : unsigned(7 downto 0) := (others => '0');
 
     begin
 
         if(clk'event and clk = '1') then
+            ctr_v := unsigned(ctr_s);
             if (state_s = processing) then
-                
-                if (unsigned(ctr) = (len - 5)) then
-                    
+                if (unsigned(ctr_s) >= len) then
                     state_s <= done;
                 else
-                    --ctr <= std_logic_vector(unsigned(ctr) + 1);
+                    --t0_s <= placeholder
+                    --t1_s <= placeholder
+                    if (unsigned(t0_s) <= 2*ETA) then 
+                        ram_in_a.data_a <= std_logic_vector(unsigned(Q) + unsigned(ETA) - unsigned(t0_s));
+                        ctr_v := ctr_v + 1;
+                    end if;
+                    if (unsigned(t1_s) <= 2*ETA and unsigned(ctr_s) < unsigned(N)) then 
+                        ram_in_a.data_b<= std_logic_vector(unsigned(Q) + unsigned(ETA) - unsigned(t1_s));
+                        ctr_v := ctr_v + 1;
+                    end if;
+                    ctr_s <= std_logic_vector(ctr_v);
+                    if (unsigned(pos_s) >= buflen) then 
+                        state_s <= done;
+                    end if;
                 end if;
             elsif(state_s = done) then
+                ctr <= unsigned(ctr_s);
+                ctr_s <= (others => '0');
+                pos_s <= (others => '0');
                 if (enable = '1') then
                     state_s <= processing;
                 end if;
@@ -55,19 +74,28 @@ begin
         end if;
     end process;
 
-    process (state_s)
+    process (state_s, ctr_s)
     
     begin
         
         case state_s is
             when done =>
                 ready <= '1';
+                ram_in_a.wren_a <= '0';
+                ram_in_a.wren_b <= '0';
             when processing =>
                 ready <= '0';
+                ram_in_a.wren_a <= '1';
+                ram_in_a.wren_b <= '1';
             when others =>
                 ready <= '0';
+                ram_in_a.wren_a <= '0';
+                ram_in_a.wren_b <= '0';
         end case;
-    
+        
+        ram_in_a.address_a <= ctr_s;
+        ram_in_a.address_b <= std_logic_vector(unsigned(ctr_s) + 1);
+
     end process;
 
 end RTL;
