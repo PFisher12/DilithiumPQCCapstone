573831112c515c9cc6d17444ae9a0a41b44ae61d
C-Carreira <167226149+C-Carreira@users.noreply.github.com>
2025-11-09 01:36:28 -0500

    working now

--- rej_eta/rej.vhd
+++ rej_eta/rej.vhd
@@ -27,14 +27,15 @@ end rej;
 architecture RTL of rej is
 
     signal ctr_s : std_logic_vector(8 downto 0);
+    signal data_addrs_s : std_logic_vector(7 downto 0);
     signal pos_s : unsigned(8 downto 0);
-    signal buf_addrs_a, buf_addrs_b : unsigned(7 downto 0);
+    signal buf_addrs_a_s, buf_addrs_b_s : unsigned(7 downto 0);
     signal pos_inner_s : unsigned(2 downto 0);
     signal data_out_a_s, data_out_b_s : std_logic_vector(31 downto 0);
     
     constant ETA : unsigned(2 downto 0) := "111";
 
-    type state is (startup, processing, done);
+    type state is (processing, done);
     signal state_s : state := done;
 
 
@@ -49,11 +50,13 @@ begin
 
         if(clk'event and clk = '1') then
             ctr_v := unsigned(ctr_s);
-            if (state_s = processing or state_s = startup) then
+            if (state_s = processing) then
+                --If all addresses have been written to, exit the loop
                 if (unsigned(ctr_s) >= len) then
                     state_s <= done;
                 else
 
+                    --Take the random buffer values to generate t0/t1
                     case pos_inner_s is
                         when "000" =>
                             t0_v := ram_out_buf.q_a(2 downto 0);
@@ -67,7 +70,7 @@ begin
                         when "011" =>
                             t0_v := ram_out_buf.q_a(26 downto 24);
                             t1_v := ram_out_buf.q_a(31 downto 29);
-                            buf_addrs_a <= buf_addrs_a + 2;
+                            buf_addrs_a_s <= buf_addrs_a_s + 2;
                         when "100" =>
                             t0_v := ram_out_buf.q_b(2 downto 0);
                             t1_v := ram_out_buf.q_b(7 downto 5);
@@ -80,11 +83,12 @@ begin
                         when "111" =>
                             t0_v := ram_out_buf.q_b(26 downto 24);
                             t1_v := ram_out_buf.q_b(31 downto 29);
-                            buf_addrs_b <= buf_addrs_b + 2;
+                            buf_addrs_b_s <= buf_addrs_b_s + 2;
                         when others =>
                             null;
                     end case;
 
+                    --If t0/t1 are within bounds, add them to the current RAM value
                     if (unsigned(t0_v) <= 2*ETA) then 
                         data_out_a_s <= std_logic_vector(unsigned(Q) + unsigned(ETA) - unsigned(t0_v));
                         ctr_v := ctr_v + 1;
@@ -95,27 +99,31 @@ begin
                         ctr_v := ctr_v + 1;
                     end if;
 
+                    --Increment the internal counter signals every cycle
                     ctr_s <= std_logic_vector(ctr_v);
                     pos_s <= pos_s + 2;
                     pos_inner_s <= pos_inner_s + 1;
 
+                    --Delay the address incrementing by 1 cycle by registering it
+                    data_addrs_s <= ctr_s(7 downto 0);
+
+                    --If the buffer is empty, exit the looop
                     if (pos_s >= buflen) then 
                         state_s <= done;
                     end if;
 
-                    state_s <= processing;
-
                 end if;
             elsif(state_s = done) then
                 ctr_s <= (others => '0');
-                buf_addrs_a <= x"00";
-                buf_addrs_b <= x"01";
+                buf_addrs_a_s <= x"00";
+                buf_addrs_b_s <= x"01";
                 data_out_a_s <= (others => '0');
                 data_out_b_s <= (others => '0');
                 pos_s <= (others => '0');
                 pos_inner_s <= "000";
+                data_addrs_s <= (others => '0');
                 if (enable = '1') then
-                    state_s <= startup;
+                    state_s <= processing;
                 end if;
             end if;
         end if;
@@ -141,13 +149,13 @@ begin
 
     end process;
 
-    ram_in_a.address_a <= ctr_s(7 downto 0);
-    ram_in_a.address_b <= std_logic_vector(unsigned(ctr_s(7 downto 0)) + 1);
+    ram_in_a.address_a <= data_addrs_s;
+    ram_in_a.address_b <= std_logic_vector(unsigned(data_addrs_s) + 1);
     ram_in_a.data_a <= data_out_a_s;
     ram_in_a.data_b <= data_out_b_s;
 
-    ram_in_buf.address_a <= std_logic_vector(buf_addrs_a);
-    ram_in_buf.address_b <= std_logic_vector(buf_addrs_b);
+    ram_in_buf.address_a <= std_logic_vector(buf_addrs_a_s);
+    ram_in_buf.address_b <= std_logic_vector(buf_addrs_b_s);
     ram_in_buf.wren_a <= '0';
     ram_in_buf.wren_b <= '0';
     ram_in_buf.data_a <= (others => '0');
